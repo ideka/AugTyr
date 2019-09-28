1aeb2cd90799eac9de0a55c7d623b42a33b90e75
Gerardo Marset <gammer1994@gmail.com>
2019-09-28 20:16:00 -0300

    Add build postprocessor to fix final exe names

---
+++ Assets/App/Logic/Editor/RenameBuild.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public static class RenameBuild
+{
+    public const string BuildName = "AugTyr";
+
+    [PostProcessBuild(1)]
+    public static void OnPostprocessBuild(BuildTarget _, string path)
+    {
+        string directory = Path.GetDirectoryName(path);
+        string builtName = Path.GetFileNameWithoutExtension(path);
+
+        File.Move(path, Path.Combine(directory, BuildName + ".exe"));
+
+        Directory.Move(
+            Path.Combine(directory, builtName + "_Data"),
+            Path.Combine(directory, BuildName + "_Data"));
+    }
+}
